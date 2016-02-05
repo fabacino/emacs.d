1bdac3f7e33f4351579c144262263b6f8f69a31c
fabacino <fabacino@gmail.com>
2016-02-05 11:32:18 +0900

    Set some org-mode settings

--- elisp/init-org.el
+++ elisp/init-org.el
@@ -1,3 +1,9 @@
+(require 'org)
+
+(global-set-key (kbd "<f12>") 'org-agenda)
+(setq org-agenda-files (list "~/org"))
+(setq org-log-done t)
+
 ;; Allow code blocks in these languages to be executed within org files
 (require 'ob-emacs-lisp)
 (require 'ob-shell)
