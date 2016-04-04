1ab53f468c7b08102c947535e631281cdf0b6822
fabacino <fabacino@gmail.com>
2016-04-04 16:06:04 +0900

    Add aliases for solarized-theme, set face for hackernews

--- elisp/init-solarized-theme.el
+++ elisp/init-solarized-theme.el
@@ -3,49 +3,51 @@
     (progn
       (require 'dired+)
       ;; Solarized palette
-      (let ((s-base03    "#002b36")
-            (s-base02    "#073642")
-            ;; emphasized content
-            (s-base01    "#586e75")
-            ;; primary content
-            (s-base00    "#657b83")
-            (s-base0     "#839496")
-            ;; comments
-            (s-base1     "#93a1a1")
-            ;; background highlight light
-            (s-base2     "#eee8d5")
-            ;; background light
-            (s-base3     "#fdf6e3")
-            
-            ;; Solarized accented colors
-            (yellow    "#b58900")
-            (orange    "#cb4b16")
-            (red       "#dc322f")
-            (magenta   "#d33682")
-            (violet    "#6c71c4")
-            (blue      "#268bd2")
-            (cyan      "#2aa198")
-            (green     "#859900")
-            
-            ;; Darker and lighter accented colors
-            ;; Only use these in exceptional circumstances!
-            (yellow-d  "#7B6000")
-            (yellow-l  "#DEB542")
-            (orange-d  "#8B2C02")
-            (orange-l  "#F2804F")
-            (red-d     "#990A1B")
-            (red-l     "#FF6E64")
-            (magenta-d "#93115C")
-            (magenta-l "#F771AC")
-            (violet-d  "#3F4D91")
-            (violet-l  "#9EA0E5")
-            (blue-d    "#00629D")
-            (blue-l    "#69B7F0")
-            (cyan-d    "#00736F")
-            (cyan-l    "#69CABF")
-            (green-d   "#546E00")
-            (green-l   "#B4C342"))
-        
+      (let* ((s-base03    "#002b36")
+             (s-base02    "#073642")
+             (s-base01    "#586e75")
+             (s-base00    "#657b83")
+             (s-base0     "#839496")
+             (s-base1     "#93a1a1")
+             (s-base2     "#eee8d5")
+             (s-base3     "#fdf6e3")
+
+             ;; Aliases
+             (primary-content s-base0)
+             (secondary-content s-base01)
+             (emphasized-content s-base1)
+             (background s-base03)
+             (background-highlights s-base02)
+
+             ;; Solarized accented colors
+             (yellow    "#b58900")
+             (orange    "#cb4b16")
+             (red       "#dc322f")
+             (magenta   "#d33682")
+             (violet    "#6c71c4")
+             (blue      "#268bd2")
+             (cyan      "#2aa198")
+             (green     "#859900")
+
+             ;; Darker and lighter accented colors
+             ;; Only use these in exceptional circumstances!
+             (yellow-d  "#7B6000")
+             (yellow-l  "#DEB542")
+             (orange-d  "#8B2C02")
+             (orange-l  "#F2804F")
+             (red-d     "#990A1B")
+             (red-l     "#FF6E64")
+             (magenta-d "#93115C")
+             (magenta-l "#F771AC")
+             (violet-d  "#3F4D91")
+             (violet-l  "#9EA0E5")
+             (blue-d    "#00629D")
+             (blue-l    "#69B7F0")
+             (cyan-d    "#00736F")
+             (cyan-l    "#69CABF")
+             (green-d   "#546E00")
+             (green-l   "#B4C342"))
+
         ;;; dired+
         ;; General
         (set-face-attribute 'diredp-dir-heading nil :background nil :foreground yellow)
@@ -66,8 +68,8 @@
         (set-face-attribute 'diredp-deletion nil :background red :foreground green-l)
         (set-face-foreground 'diredp-deletion-file-name red)
         (set-face-foreground 'diredp-mode-line-flagged red)
-        (set-face-attribute 'diredp-flag-mark nil :background green-l :foreground s-base03)
-        (set-face-attribute 'diredp-flag-mark-line nil :background s-base01 :foreground s-base03)
+        (set-face-attribute 'diredp-flag-mark nil :background green-l :foreground background)
+        (set-face-attribute 'diredp-flag-mark-line nil :background s-base01 :foreground background)
         (set-face-foreground 'diredp-mode-line-marked green-l)
         
 ;;   `diredp-compressed-file-suffix'
@@ -79,8 +81,14 @@
         
         ;;; ace-window
         (set-face-foreground 'aw-leading-char-face red)
-        (set-face-background 'aw-background-face s-base03)
-        (set-face-foreground 'aw-background-face s-base01))))
+        (set-face-foreground 'aw-background-face secondary-content)
+
+        ;;; hackernews
+        (set-face-foreground 'hackernews-link-face emphasized-content)
+        (set-face-foreground 'hackernews-comment-count-face secondary-content)
+        (set-face-foreground 'hackernews-score-face primary-content)
+
+        )))
 
 
 ;(setq solarized-distinct-fringe-background t)
