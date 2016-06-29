1ed630986cc97ad3760d80cec031476a5fb39489
fabacino <fabacino@gmail.com>
2016-06-29 14:24:12 +0200

    Add package w32-browser

---
+++ elpa/w32-browser-20151231.1620/w32-browser-pkg.el
@@ -0,0 +1,1 @@
+(define-package "w32-browser" "20151231.1620" "Run Windows application associated with a file." 'nil :url "http://www.emacswiki.org/w32-browser.el" :keywords '("mouse" "dired" "w32" "explorer"))
