26d62e1937fd90bdc007b6a02a46857b376b25a2
fabacino <fabacino@gmail.com>
2016-08-18 15:45:01 +0200

    Update elpa packages

--- elpa/w32-browser-20151231.1620/w32-browser-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; w32-browser-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("w32-browser.el") (22387 47808 240838
-;;;;;;  400000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; w32-browser-autoloads.el ends here
