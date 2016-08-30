89ecca3ffb3e7f891b59e4f5ca92185569021a78
fabacino <fabacino@gmail.com>
2016-08-30 13:22:42 +0200

    Replace obsolete function

--- elisp/init-w3m.el
+++ elisp/init-w3m.el
@@ -2,7 +2,7 @@
 (global-set-key (kbd "<f8>") 'w3m-goto-url-new-session)
 (setq browse-url-browser-function 'w3m-goto-url-new-session)
 (defun fbn/w3m-hook ()
-  (local-set-key (kbd "M") 'w3m-external-view-current-url))
+  (local-set-key (kbd "M") 'w3m-view-url-with-browse-url))
 (add-hook 'w3m-mode-hook 'fbn/w3m-hook)
 
 
