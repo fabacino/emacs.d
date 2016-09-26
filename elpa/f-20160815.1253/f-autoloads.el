f775dc1cbaa2386aee4e3a20b270f874ab444730
fabacino <fabacino@gmail.com>
2016-09-26 09:09:56 +0200

    Update elpa packages

--- elpa/f-20160815.1253/f-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; f-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("f.el") (22453 47808 793991 300000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; f-autoloads.el ends here
--- elpa/w3m-20160817.1031/w3m-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "w3m" "20160817.1031" "an Emacs interface to w3m" 'nil :keywords
-  '("w3m" "www" "hypermedia"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
