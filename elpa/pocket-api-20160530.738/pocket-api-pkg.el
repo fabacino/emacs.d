8e95035cd2b465039b336d4580a2ffede35f1e1d
fabacino <fabacino@gmail.com>
2016-10-25 08:31:14 +0200

    Update elpa packages

---
+++ elpa/pocket-api-20160530.738/pocket-api-pkg.el
@@ -0,0 +1,1 @@
+(define-package "pocket-api" "20160530.738" "another pocket api" '((emacs "24.4") (request "0.2")) :url "https://github.com/lujun9972/pocket-api.el" :keywords '("convenience" "pocket"))
