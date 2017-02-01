3a52dc15dbd4e0d785ff5ee0aa40d2477bfa0462
fabacino <fabacino@gmail.com>
2017-02-01 14:50:50 +0100

    Update elpa packages

--- elpa/w32-browser-20160814.1433/w32-browser-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "w32-browser" "20160814.1433" "Run Windows application associated with a file." 'nil :url "http://www.emacswiki.org/w32-browser.el" :keywords '("mouse" "dired" "w32" "explorer"))
