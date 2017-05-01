f7c22c1cfd69054b8d60e5754930ec6a3b62e4f5
fabacino <fabacino@gmail.com>
2017-05-01 13:15:00 +0200

    Put url and title into w3m buffer names

--- elisp/init-w3m.el
+++ elisp/init-w3m.el
@@ -1,9 +1,17 @@
 (require 'w3m)
+
 (global-set-key (kbd "<f8>") 'w3m-goto-url-new-session)
 (setq browse-url-browser-function 'w3m-goto-url-new-session)
+
 (defun fbn/w3m-hook ()
   (local-set-key (kbd "M") 'w3m-view-url-with-browse-url))
 (add-hook 'w3m-mode-hook 'fbn/w3m-hook)
+
+(defun fbn/w3m-rename-buffer (url)
+  "Put url and title into the buffer name."
+  (let ((name (concat "w3m - " w3m-current-title " [" w3m-current-url "]")))
+    (rename-buffer name t)))
+(add-hook 'w3m-display-hook 'fbn/w3m-rename-buffer)
 
 
 (provide 'init-w3m)
