51c3b192229971a9d39fcd03ac1de8741a27a0ac
fabacino <fabacino@gmail.com>
2017-07-24 09:18:45 +0200

    Update elpa packages

--- elpa/geben-20170125.1937/geben-pkg.el
+++
@@ -1,8 +0,0 @@
-(define-package "geben" "20170125.1937" "DBGp protocol frontend, a script debugger"
-  '((emacs "24.3")
-    (cl-lib "0.5"))
-  :url "https://github.com/ahungry/geben" :keywords
-  '("c" "comm" "tools"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
---
+++ elpa/geben-20170620.2012/geben-pkg.el
@@ -0,0 +1,8 @@
+(define-package "geben" "20170620.2012" "DBGp protocol frontend, a script debugger"
+  '((emacs "24.3")
+    (cl-lib "0.5"))
+  :url "https://github.com/ahungry/geben" :keywords
+  '("c" "comm" "tools"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
