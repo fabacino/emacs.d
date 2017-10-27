e4dfb504cdea1be5747e12210cc473fc1c8b94c8
fabacino <fabacino@gmail.com>
2017-10-27 08:08:03 +0200

    Add workaround for projectile issue #1183
    https://github.com/bbatsov/projectile/issues/1183

--- elisp/init-projectile.el
+++ elisp/init-projectile.el
@@ -2,5 +2,10 @@
 (require 'counsel-projectile)
 (projectile-global-mode)
 
+;; Workaround, see https://github.com/bbatsov/projectile/issues/1183
+(setq projectile-mode-line
+      '(:eval (format " Projectile[%s]"
+                      (projectile-project-name))))
+
 
 (provide 'init-projectile)
