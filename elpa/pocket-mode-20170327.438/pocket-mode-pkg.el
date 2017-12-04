895cb8ea825368b17d6d8728bbed8cd1637a1da7
fabacino <fabacino@gmail.com>
2017-12-04 09:57:50 +0100

    Update elpa packages

--- elpa/pocket-mode-20170327.438/pocket-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "pocket-mode" "20170327.438" "Manage your pocket" '((emacs "24.4") (pocket-api "0.1")) :keywords '("convenience" "pocket"))
---
+++ elpa/pocket-mode-20171201.515/pocket-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "pocket-mode" "20171201.515" "Manage your pocket" '((emacs "24.4") (pocket-api "0.1")) :commit "229de7d35b7e5605797591c46aa8200d7efc363c" :keywords '("convenience" "pocket"))
