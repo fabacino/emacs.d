ad87c35d14e95878f94dbb5467ecede41f72d59b
fabacino <fabacino@gmail.com>
2019-10-22 23:21:52 +0200

    Update elpa packages

--- elpa/counsel-20190527.1406/counsel-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "counsel" "20190527.1406" "Various completion functions using Ivy" '((emacs "24.3") (swiper "0.11.0")) :commit "332f990fc1b68f84ebb9d0557ce4abac8392cd79" :keywords '("convenience" "matching" "tools") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
--- elpa/swiper-20190528.1333/swiper-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "swiper" "20190528.1333" "Isearch with an overview. Oh, man!" '((emacs "24.1") (ivy "0.11.0")) :commit "332f990fc1b68f84ebb9d0557ce4abac8392cd79" :keywords '("matching") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
