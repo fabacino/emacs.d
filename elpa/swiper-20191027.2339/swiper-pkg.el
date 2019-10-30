ed787da982fc2f4ab4c0ea4652f8dba5b5eed403
fabacino <fabacino@gmail.com>
2019-10-30 15:25:58 +0100

    Update elpa packages

---
+++ elpa/swiper-20191027.2339/swiper-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "swiper" "20191027.2339" "Isearch with an overview. Oh, man!" '((emacs "24.5") (ivy "0.13.0")) :commit "256deea51357085b7421c1fefcdeb97c77e2ca57" :keywords '("matching") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
