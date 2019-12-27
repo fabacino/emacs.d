e54dab20056fd03e15d5af6c7ec5a51478185ce8
fabacino <fabacino@gmail.com>
2019-12-27 11:09:28 +0100

    Update elpa packages

---
+++ elpa/find-file-in-project-20191223.2328/find-file-in-project-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "find-file-in-project" "20191223.2328" "Find file/directory and review Diff/Patch/Commit efficiently everywhere" '((ivy "0.10.0") (emacs "24.4")) :commit "4540416d5aaf7f6adcdcea3ac5b80a08bb695e44" :keywords '("project" "convenience") :authors '(("Phil Hagelberg, Doug Alcorn, and Will Farrington")) :maintainer '("Chen Bin" . "chenbin.sh@gmail.com") :url "https://github.com/technomancy/find-file-in-project")
