9ec20cf0ec28a0d76420fed5ad5b6aa68e74c699
fabacino <fabacino@gmail.com>
2020-08-24 15:15:18 +0200

    Update elpa packages

--- elpa/git-commit-20200207.1819/git-commit-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "git-commit" "20200207.1819" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (transient "20190812") (with-editor "20181103")) :commit "d27d6e467857ed4a78c7cf7d609561df789e2a6c" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
