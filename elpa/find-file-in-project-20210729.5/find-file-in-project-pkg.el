559c46edfb6a34e45cadd93a12ee4f2db29dd725
fabacino <fabacino@gmail.com>
2021-08-03 12:20:52 +0200

    Update elpa packages

---
+++ elpa/find-file-in-project-20210729.5/find-file-in-project-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from find-file-in-project.el  -*- no-byte-compile: t -*-
+(define-package "find-file-in-project" "20210729.5" "Find file/directory and review Diff/Patch/Commit efficiently everywhere" '((emacs "25.1")) :commit "136018df39520433840d67acbbf92908ab115532" :authors '(("Phil Hagelberg, Doug Alcorn, and Will Farrington")) :maintainer '("Chen Bin" . "chenbin.sh@gmail.com") :keywords '("project" "convenience") :url "https://github.com/redguardtoo/find-file-in-project")
---
+++ elpa/helm-core-20210801.459/helm-core-pkg.el
@@ -0,0 +1,7 @@
+(define-package "helm-core" "20210801.459" "Development files for Helm"
+  '((emacs "25.1")
+    (async "1.9.4"))
+  :commit "1b6ffb10b8cabf177664a34827961520fba88ebe")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
