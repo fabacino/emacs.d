7168b93cb71628d5146e72368e5a1693d6be7b09
fabacino <fabacino@gmail.com>
2022-02-17 18:14:15 +0100

    Update elpa packages

---
+++ elpa/emms-20220131.501/emms-pkg.el
@@ -0,0 +1,14 @@
+(define-package "emms" "20220131.501" "The Emacs Multimedia System"
+  '((cl-lib "0.5")
+    (nadvice "0.3")
+    (seq "0"))
+  :commit "d32a9bbc8ffcd736c4aa33a6dcea819a01b8e808" :authors
+  '(("Jorgen Schäfer" . "forcer@forcix.cx"))
+  :maintainer
+  '("Yoni Rabkin" . "yrk@gnu.org")
+  :keywords
+  '("emms" "mp3" "ogg" "flac" "music" "mpeg" "video" "multimedia")
+  :url "https://www.gnu.org/software/emms/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/helm-20220216.1254/helm-pkg.el
@@ -0,0 +1,11 @@
+(define-package "helm" "20220216.1254" "Helm is an Emacs incremental and narrowing framework"
+  '((helm-core "3.8.4")
+    (popup "0.5.3"))
+  :commit "c9e1c5b964ffd0a0fa635f501adc5457914283a4" :authors
+  '(("Thierry Volpiatto" . "thierry.volpiatto@gmail.com"))
+  :maintainer
+  '("Thierry Volpiatto" . "thierry.volpiatto@gmail.com")
+  :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
