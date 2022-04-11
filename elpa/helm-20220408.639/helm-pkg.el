d1b19020e7882a5e18c6b05a3b28dc5d0687b3fb
fabacino <fabacino@gmail.com>
2022-04-11 09:51:26 +0200

    Update elpa packages

---
+++ elpa/helm-20220408.639/helm-pkg.el
@@ -0,0 +1,11 @@
+(define-package "helm" "20220408.639" "Helm is an Emacs incremental and narrowing framework"
+  '((helm-core "3.8.4")
+    (popup "0.5.3"))
+  :commit "cac2ffceefb816a3dcb005ed2d4e1fc8b2450669" :authors
+  '(("Thierry Volpiatto" . "thierry.volpiatto@gmail.com"))
+  :maintainer
+  '("Thierry Volpiatto" . "thierry.volpiatto@gmail.com")
+  :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
